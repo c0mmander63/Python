7cb3c61517f4dce40967a602538d093b6dd34ece
c0mmander63 <79047348+c0mmander63@users.noreply.github.com>
2021-10-08 19:20:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#Variables
+
+a = 1 
+
+b = 2 
+
+# Here a is not containing the value "1" (Like c or c++)
+# a is just referring the value '1'
+
+c = 1 
+
+# Now even C refers to 1 
+
+# print (c) = 1
+
+f = c 
+
+# now f refers to 1 not the variable C
+
+# print (f) = 1 
+
+c = 4
+
+# Now c referrring to 4 i.e c = 4,  but f remains reffering '1', which is previous value of c; f = 1 
+
+
